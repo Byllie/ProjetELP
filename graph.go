d93bd24a680037de73d44bea79e19158c35e692b
Byllie <vantourout.gwendal@gmail.com>
2025-01-14 00:20:32 +0100

    Ajout du preprocessing du graph

--- graph.go
+++ graph.go
@@ -8,6 +8,11 @@ import (
 )
 
 type Graph struct {
+	Vertices    map[int]*Vertex
+	Communities []*Community
+}
+
+type Community struct {
 	Vertices map[int]*Vertex
 }
 
@@ -27,11 +32,63 @@ func (graph *Graph) AddEdge(srcKey, destKey int) {
 	graph.Vertices[destKey].Edges[srcKey] = graph.Vertices[srcKey]
 }
 
+func (graph *Graph) CountTrianglesVertices(node int) int {
+	if _, ok := graph.Vertices[node]; !ok {
+		return 0
+	}
+
+	count := 0
+	neighbors := graph.Vertices[node].Edges
+
+	for neighbor1 := range neighbors {
+		for neighbor2 := range neighbors {
+			if neighbor1 < neighbor2 && graph.Vertices[neighbor1].Edges[neighbor2] != nil {
+				count++
+			}
+		}
+	}
+	return count / 2
+}
+
+func (graph *Graph) CountTrianglesEdge(src, dest int) int {
+	if _, ok := graph.Vertices[src]; !ok {
+		return 0
+	}
+	if _, ok := graph.Vertices[dest]; !ok {
+		return 0
+	}
+
+	count := 0
+	for neighbor := range graph.Vertices[src].Edges {
+		if neighbor != dest && graph.Vertices[dest].Edges[neighbor] != nil {
+			count++
+		}
+	}
+	return count
+}
+
+func (graph *Graph) RemoveEdgesWithoutTriangles() {
+	for src, vertex := range graph.Vertices {
+		for dest := range vertex.Edges {
+			if src < dest {
+				if graph.CountTrianglesEdge(src, dest) == 0 {
+					delete(graph.Vertices[src].Edges, dest)
+					delete(graph.Vertices[dest].Edges, src)
+				}
+			}
+		}
+	}
+}
+
+func (graph *Graph) WccCommunity(node int, c Community) float32 {
+	return 0.1
+}
+
 func NewGraphFromFile(filePath string) *Graph {
 	file, _ := os.Open(filePath)
 	defer file.Close()
 
-	graph := &Graph{Vertices: make(map[int]*Vertex)}
+	graph := &Graph{Vertices: make(map[int]*Vertex), Communities: []*Community{}}
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
@@ -40,7 +97,6 @@ func NewGraphFromFile(filePath string) *Graph {
 		if strings.HasPrefix(line, "#") || strings.TrimSpace(line) == "" {
 			continue
 		}
-
 		parts := strings.Fields(line)
 		if len(parts) != 2 {
 			continue
@@ -58,4 +114,5 @@ func NewGraphFromFile(filePath string) *Graph {
 func main() {
 	filePath := "graph.txt"
 	graph := NewGraphFromFile(filePath)
+	graph.RemoveEdgesWithoutTriangles()
 }
