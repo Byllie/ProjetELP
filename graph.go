ef16f0a0de5265b13d6b0b0feeb81f8870436605
Byllie <vantourout.gwendal@gmail.com>
2025-01-13 15:36:49 +0100

    Créeation fonction manipulation graph <3

--- graph.go
+++ graph.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type Graph struct {
 	Vertices map[int]*Vertex
 }
@@ -8,14 +15,47 @@ type Vertex struct {
 	Edges map[int]*Vertex
 }
 
-func (graph *Graph) AddEdge(srcKey, destKey int, weight int) {
+func (graph *Graph) AddEdge(srcKey, destKey int) {
 	if _, ok := graph.Vertices[srcKey]; !ok {
-		graph.Vertices[srcKey] = &Vertex{map[int]*Vertex{}}
+		graph.Vertices[srcKey] = &Vertex{make(map[int]*Vertex)}
 	}
 	if _, ok := graph.Vertices[destKey]; !ok {
-		graph.Vertices[destKey] = &Vertex{map[int]*Vertex{}}
+		graph.Vertices[destKey] = &Vertex{make(map[int]*Vertex)}
 	}
 
 	graph.Vertices[srcKey].Edges[destKey] = graph.Vertices[destKey]
 	graph.Vertices[destKey].Edges[srcKey] = graph.Vertices[srcKey]
+}
+
+func NewGraphFromFile(filePath string) *Graph {
+	file, _ := os.Open(filePath)
+	defer file.Close()
+
+	graph := &Graph{Vertices: make(map[int]*Vertex)}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.HasPrefix(line, "#") || strings.TrimSpace(line) == "" {
+			continue
+		}
+
+		parts := strings.Fields(line)
+		if len(parts) != 2 {
+			continue
+		}
+
+		srcKey, _ := strconv.Atoi(parts[0])
+
+		destKey, _ := strconv.Atoi(parts[1])
+
+		graph.AddEdge(srcKey, destKey)
+	}
+	return graph
+}
+
+func main() {
+	filePath := "graph.txt"
+	graph := NewGraphFromFile(filePath)
 }
