8ef323982b981e345b7d0d5e1257f4ef4a34b0e6
Byllie <vantourout.gwendal@gmail.com>
2025-02-01 10:27:33 +0100

    rewrite worker et implémentation tcp

--- Go/worker.go
+++ Go/worker.go
@@ -1,49 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+	"time"
+)
 
-func worker(id int, graph *Graph, jobs <-chan int, results chan<- int, done chan struct{}) {
-	for {
-		select {
-		case j, ok := <-jobs:
-			if !ok {
-				close(done)
-				return
-			}
-
-			results <- j * 2
-		case <-done:
-			return
-		}
+func worker(id int, jobs <-chan rune, results chan<- bool) {
+	for char := range jobs {
+		fmt.Printf("Le worker %d travaille sur le charactère: %c\n", id, char)
+		time.Sleep(6 * time.Second)
+		results <- true
 	}
 }
 
-func create_worker() {
-	const numWorkers = 5
-	jobs := make(chan int, numWorkers*2)    // Numéro du node dans le graph
-	results := make(chan int, numWorkers*2) //-1 remove; 0 no action; x transfer to comunity x
-	done := make(chan struct{})
-	graph := &Graph{
-		Vertices: map[int]*Vertex{
-			1: {Edges: map[int]*Vertex{2: {}, 3: {}}},
-			2: {Edges: map[int]*Vertex{1: {}, 3: {}}},
-			3: {Edges: map[int]*Vertex{1: {}, 2: {}}},
-		},
-	}
-
-	for i := 1; i <= numWorkers; i++ {
-		go worker(i, graph, jobs, results, done)
-	}
-
-	for node := range graph.Vertices {
-		jobs <- node
+func createJobs(input string, jobs chan<- rune) {
+	for _, char := range input {
+		jobs <- char
 	}
 	close(jobs)
+}
 
-	for a := 1; a <= 9; a++ {
-		result := <-results
-		fmt.Println("Result:", result)
+func processString(input string, numWorkers int) {
+	jobs := make(chan rune, 2*numWorkers)
+	results := make(chan bool, 2*numWorkers)
+
+	for w := 1; w <= numWorkers; w++ {
+		go worker(w, jobs, results)
 	}
 
-	<-done
+	go createJobs(input, jobs)
+
+	for a := 1; a <= len(input); a++ {
+		<-results
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Erreur de lecture:", err)
+		return
+	}
+	input = strings.TrimSpace(input)
+	fmt.Printf("Recu: %s\n", input)
+	const numWorkers = 3
+	processString(input, numWorkers)
+	fmt.Println("Fini:", input)
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":5827")
+	if err != nil {
+		fmt.Println("Erreur dans le démarage du serveur:", err)
+		return
+	}
+	defer listener.Close()
+	fmt.Println("Server is listening on port 5827...")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
 }
